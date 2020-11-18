201eb1df622665c4188861819c81ea65cb5cb065
Jonah Björnfot <65647056+JonahBjornfot@users.noreply.github.com>
2020-11-18 14:02:23 +0100

    Vecka 47. Onsdag

---
+++ 47/1/script.js
@@ -0,0 +1,7 @@
+let i = 0
+
+while (i < 6)
+     console.log(i)
+     a++;
+     
+console.log("Färdig!")
---
+++ 47/2/script.js
@@ -0,0 +1,14 @@
+const sumThree = (x,y,z) => {
+    console.log(x + y + z)
+}
+
+sumThree(2,5,10);
+
+const isZero = (x) => {
+    if(x === 0){
+        console.log(true)
+    }
+    else{
+        console.log(false)
+    }
+}
